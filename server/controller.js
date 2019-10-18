a382397ce5b29093661bd0ecd2569b0ae7120114
jnjagod <josh.jagoda@gmail.com>
2019-10-18 08:03:57 -0700

    adjusted indents

--- server/controller.js
+++ server/controller.js
@@ -1,40 +1,40 @@
 const people = require('./data')
 
 module.exports = {
-    getAllPeople(req, res, next) {
-        // console.log(people)
-        res.status(200).send(people)
-    },
-    getAllFemales(req, res, next) {
-        let females = people.filter(el => {
-            return (el.gender === "F")
-        })
-        // console.log(females)
-        res.status(200).send(females)
-    },
-    getAllMales(req, res, next) {
-        let males = people.filter(el => {
-            return (el.gender === "M")
-        })
-        // console.log(males)
-        res.status(200).send(males)
-    },
-    getPeopleByCountry(req, res, next) {
-        // console.log(req.query)
-        const {count} = req.query
-        let countries = people.filter(el => {
-           return el.country.toLowerCase().includes(count.toLowerCase())
-        })
-        res.status(200).send(countries)
-        // console.log(countries)
-    },
-    getPeopleByGenre(req, res, next) {
-        console.log(req.query)
-        const {gen} = req.query
-        let genres = people.filter(el => {
-           return el.movie_genre.toLowerCase().includes(gen.toLowerCase())
-        })
-        res.status(200).send(genres)
-        console.log(genres)
-    }
+  getAllPeople(req, res, next) {
+    // console.log(people)
+    res.status(200).send(people)
+  },
+  getAllFemales(req, res, next) {
+    let females = people.filter(el => {
+      return (el.gender === "F")
+    })
+    // console.log(females)
+    res.status(200).send(females)
+  },
+  getAllMales(req, res, next) {
+    let males = people.filter(el => {
+      return (el.gender === "M")
+    })
+    // console.log(males)
+    res.status(200).send(males)
+  },
+  getPeopleByCountry(req, res, next) {
+    // console.log(req.query)
+    const {count} = req.query
+    let countries = people.filter(el => {
+      return el.country.toLowerCase().includes(count.toLowerCase())
+    })
+    res.status(200).send(countries)
+    // console.log(countries)
+  },
+  getPeopleByGenre(req, res, next) {
+    console.log(req.query)
+    const {gen} = req.query
+    let genres = people.filter(el => {
+      return el.movie_genre.toLowerCase().includes(gen.toLowerCase())
+    })
+    res.status(200).send(genres)
+    console.log(genres)
+  }
 }
